0e1d4dcd1e87b017cf3eb33839fa64febf40f6ba
Turbonalle <jonathanbagger@hotmail.com>
2022-11-24 20:21:16 +0200

    factorizer

---
+++ prime_numbers.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+void factorize(int n)
+{
+	int i;
+	
+	i = 2;
+	if (n > 0)
+	{
+		if (n < 2)
+			printf("%d", n);
+		else
+		{
+			while ((i * i) <= n)
+			{
+				while (n % i == 0)
+				{
+					printf("%d ", i);
+					n /= i;
+				}
+				i++;
+			}
+			if (n != 1)
+				printf("%d", n);
+		}
+	}
+}
+
+int main(int ac, char **av)
+{
+	if (ac != 2)
+		return (0);
+	factorize(atoi(av[1]));
+	return (0);
+}
