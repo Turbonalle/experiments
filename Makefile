df33345f2ac18403d58f08d29e47a09b779956a3
jbagger <jbagger@student.hive.fi>
2022-11-25 08:43:36 +0200

    created folder system and makefile

---
+++ Makefile
@@ -0,0 +1,9 @@
+m1:
+	@gcc Math/m1_6174.c
+
+m2:
+	@gcc Math/m2_factorizer.c
+
+clean:
+	@rm *.out
+	@echo "All clean!"
--- Math/m2_factorizer.c
+++ Math/m2_factorizer.c
@@ -32,5 +32,6 @@ int main(int ac, char **av)
 	if (ac != 2)
 		return (0);
 	factorize(atoi(av[1]));
+	printf("\n");
 	return (0);
 }
