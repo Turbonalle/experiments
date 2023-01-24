7112ab2e6c920e1a9ba8ab8c72c72056c48e6052
Turbonalle <jonathanbagger@hotmail.com>
2023-01-24 20:28:17 +0200

    Started on base converter + renamed som files

---
+++ Math/m3_euclidean_algorithm.c
@@ -0,0 +1,18 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+int euclids_algorithm(int dividend, int divisor)
+{
+	if (divisor == 0) return (dividend);
+	int remainder = dividend % divisor;
+	printf("%d\t%d\t%d\n", dividend, divisor, remainder);
+	return (euclids_algorithm(divisor, remainder));
+}
+
+int main(int ac, char *av[])
+{
+	printf("divid\tdivis\tremain\n");
+	if (ac == 3)
+		printf("GCF = %d\n", euclids_algorithm(atoi(av[1]), atoi(av[2])));
+	return (0);
+}
---
+++ Math/m5_num_converter.c
@@ -0,0 +1,58 @@
+typedef struct data
+{
+	char	*string;
+	char	base;
+	int		tries;
+}			data;
+
+void welcome_text(void)
+{
+	printf("I will convert any number for you!\n");
+	printf("Tell me which number system to convert from, by writing the following characters:\n");
+	printf("Binary:\tb\n");
+	printf("Binary:\to\n");
+	printf("Binary:\td\n");
+	printf("Binary:\th\n");
+}
+
+int get_base_input(data *data)
+{
+	char c;
+
+	scanf("%c", c);
+	if (c != 'b' && c != 'o' && c != 'd' && c != 'h')
+	{
+		printf("Incorrect input! Please try again.\n");
+		return (0);
+	}
+	data->base = c;
+	return (1);
+}
+
+int program(data *data)
+{
+	welcome_text();
+	if (!get_base_input(data))
+	{
+		data->tries++;
+		if (data->tries == 3)
+		{
+			printf("Failed to enter correct input 3 times!\nExiting...\n");
+			return (0);
+		}
+		program(data);
+	}
+	get_number_input(data);
+	handle_input(data);
+	return (1);
+}
+
+int main()
+{
+	data data;
+
+	data.tries = 0;
+	program(&data);
+	free(&data);
+	return (0);
+}
