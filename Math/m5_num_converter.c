566c83c81009456e225269febd84430447c1593c
jbagger <jbagger@student.hive.fi>
2023-01-25 11:08:39 +0200

    converter works with decimal input

--- Math/m5_num_converter.c
+++ Math/m5_num_converter.c
@@ -1,37 +1,86 @@
+#include <unistd.h>
+#include <stdio.h>
+#include <stdlib.h>
+
 typedef struct data
 {
-	char	*string;
-	char	base;
-	int		tries;
-}			data;
+	int	number;
+	int	base;
+	int	tries;
+}		data;
 
 void welcome_text(void)
 {
-	printf("I will convert any number for you!\n");
+	printf("I will convert any positive number for you!\n");
 	printf("Tell me which number system to convert from, by writing the following characters:\n");
-	printf("Binary:\tb\n");
-	printf("Octal:\to\n");
-	printf("Decimal:\td\n");
-	printf("Hexa:\th\n");
+	printf("b : Binary\n");
+	printf("o : Octal\n");
+	printf("d : Decimal\n");
+	printf("h : Hexadecimal\n\n");
 }
 
 int get_base_input(data *data)
 {
-	char c;
+	char	c[1];
+	int		base;
 
-	scanf("%c", c);
-	if (c != 'b' && c != 'o' && c != 'd' && c != 'h')
-	{
-		printf("Incorrect input! Please try again.\n");
+	scanf("%s", c);
+	if (c[0] != 'b' && c[0] != 'o' && c[0] != 'd' && c[0] != 'h')
 		return (0);
-	}
-	data->base = c;
+	if (c[0] == 'b')
+		data->base = 2;
+	else if (c[0] == 'o')
+		data->base = 8;
+	else if (c[0] == 'd')
+		data->base = 10;
+	else
+		data->base = 16;
 	return (1);
+}
+void get_number_input(data *data)
+{
+	char input[32];
+
+	printf("Enter number you want to convert: ");
+	scanf("%s", input);
+	write(1, "\n", 1);
+	data->number = atoi(input);
+}
+
+void convert_decimal(data *data, int base)
+{
+	char *hex_numbers;
+	char buffer[50];
+	char *result;
+	int n;
+
+	n = data->number;
+	hex_numbers = "0123456789abcdef";
+	result = &buffer[49];
+	*result = '\0';
+	while (n > 0)
+	{
+		*--result = hex_numbers[n % base];
+		n /= base;
+	}
+	while (*result)
+	{
+		write(1, &(*result), 1);
+		result++;
+	}
+	write(1, "\n", 1);
 }
 
 void handle_input(data *data)
 {
-	
+	write(1, "Binary      : ", 14);
+	convert_decimal(data, 2);
+	write(1, "Octal       : ", 14);
+	convert_decimal(data, 8);
+	write(1, "Decimal     : ", 14);
+	convert_decimal(data, 10);
+	write(1, "Hexadecimal : ", 14);
+	convert_decimal(data, 16);
 }
 
 int program(data *data)
@@ -43,10 +92,12 @@ int program(data *data)
 		if (data->tries == 3)
 		{
 			printf("Failed to enter correct input 3 times!\nExiting...\n");
-			return (0);
+			exit(EXIT_FAILURE);
 		}
+		printf("Incorrect input! Please try again.\n");
 		program(data);
 	}
+	write(1, "\n", 1);
 	get_number_input(data);
 	handle_input(data);
 	return (1);
@@ -58,6 +109,5 @@ int main()
 
 	data.tries = 0;
 	program(&data);
-	free(&data);
 	return (0);
 }
