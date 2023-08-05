91d8ceb66085db0893def1c2cfa8da80fa51a02e
Turbonalle <jonathanbagger@hotmail.com>
2023-08-06 02:03:01 +0300

    logic starting to work, need to fix pointer issues

--- Programs/SatisfactoryCalculator/main.c
+++ Programs/SatisfactoryCalculator/main.c
@@ -1,48 +1,133 @@
 #include "satisfactory_calculator.h"
 
-void	init_buildings(data *d)
+//------------------------------------------------------------------------------
+
+void init_items(t_data *d)
 {
-	// building: { name, power_usage, items_needed }
-	t_building	smelter = { "smelter", 4, 1 };
-	t_building	constructor = { "constructor", 4, 1 };
+	t_item	iron_ore = { ORE, 0, 0, 0 };
+	t_item	iron_ingot = { ITEM, 1, 0, 0 };
 
-	d.buildings[0] = smelter;
-
+	d->items[IRON_ORE] = iron_ore;
+	d->items[IRON_INGOT] = iron_ingot;
 }
 
-void	init_items(data *d)
-{
-	t_recipe	iron_ingot;
+//------------------------------------------------------------------------------
 
-	iron_ingot.product = "iron_ingot";
-	iron_ingot.production_amount = 30;
-	iron_ingot.second_interval = 2;
-	iron_ingot.building = d.buildings.smelter;
-	
-	d.recipes[0] = iron_ingot;
+void init_buildings(t_data *d)
+{
+	t_building	smelter = { "Smelter", 4, 1 };
+	t_building	constructor = { "Constructor", 4, 1 };
+	t_building	manufacturer = { "Manufacturer", 4, 1 };
+	t_building	coal_generator = { "Coal Generator", 4, 1 };
+	t_building	biomass_burner = { "Biomass Burner", 4, 1 };
+	t_building	coal_power_plant = { "Coal Power Plant", 4, 1 };
+	t_building	oil_refinery = { "Oil Refinery", 4, 1 };
+	t_building	compactor = { "Compactor", 4, 1 };
+	t_building	assembler = { "Assembler", 4, 1 };
+	t_building	foundry = { "Foundry", 4, 1 };
+
+	d->buildings[SMELTER] = smelter;
+	d->buildings[CONSTRUCTOR] = constructor;
 }
 
-void	get_player_requirement(data *d)
+//------------------------------------------------------------------------------
+
+void	get_requirements(t_data *d, t_item *item)
 {
-	d.player_requirement.item = "iron_ingot";
-	d.player_requirement.amount_needed = 60;
+	item->recipes[0].items_needed[0].amount_needed = item->amount_needed / item->recipes[0].production_amount * item->recipes[0].item_amount_needed[0];
+}
+
+//------------------------------------------------------------------------------
+
+void	calculate_power_consumption(t_data *d)
+{
+	int i;
+
+	d->power_consumption = 0;
+	i = -1;
+	while (++i < ITEMS)
+	{
+		if (d->items[i].type == ITEM)
+		{
+			d->power_consumption += ceil(d->items[i].amount_needed / d->items[i].recipes[0].production_amount) * d->items[i].recipes[0].building.power_usage_MW;
+		}
+		else if (d->items[i].type == ORE)
+		{
+			d->power_consumption += ceil(d->items[i].amount_needed / d->items[i].recipes[0].production_amount) * d->items[i].recipes[0].building.power_usage_MW;
+		}
+	}
+}
+
+//------------------------------------------------------------------------------
+
+void print_item_requirements(t_data *d, t_item *item)
+{
+	int i;
+
+	i = -1;
+	while (++i < ITEMS)
+	{
+		if (d->items[i].type == ITEM)
+			printf("%s: %d\n", d->items[i].recipes[0].items_needed[0].ore, d->items[i].recipes[0].items_needed[0].amount_needed);
+	}
+}
+
+//------------------------------------------------------------------------------
+
+void print_power_consumption(t_data *d)
+{
+	printf("Power consumption: %d MW\n", d->power_consumption);
+}
+
+//------------------------------------------------------------------------------
+
+void create_iron_ingot_recipe(t_data *d)
+{
+	t_recipe iron_ingot_recipe;
+
+	iron_ingot_recipe.production_amount = 30;
+	iron_ingot_recipe.second_interval = 2;
+	iron_ingot_recipe.building = d->buildings[SMELTER];
+	iron_ingot_recipe.items_needed[0] = d->items[IRON_ORE];
+	iron_ingot_recipe.item_amount_needed[0] = 30;
+
+	// Add recipe to list
+	d->items[IRON_INGOT].recipes[0] = iron_ingot_recipe;
 }
 
 int main(void)
 {
 	t_data d;
 
-	init_buildings(&d);
 	init_items(&d);
+	init_buildings(&d);
 
-	get_player_requirement(&d);
-	calculate_requirements(&d);
+	t_item	iron_ore = { ORE, 0, 0, 0 };
+	t_item	iron_ingot = { ITEM, 1, 0, 0 };
+	t_recipe iron_ingot_recipe;
+
+	// Initialize amount needed to 0
+	iron_ore.amount_needed = 0;
+	iron_ingot.amount_needed = 0;
+
+	// Create recipe for iron ingot
+	create_iron_ingot_recipe(&d);
+	iron_ingot_recipe.production_amount = 30;
+	iron_ingot_recipe.second_interval = 2;
+	iron_ingot_recipe.building = d.buildings[SMELTER];
+
+	// Add recipe to list
+	iron_ingot.recipes[0] = iron_ingot_recipe;
+
+	iron_ingot.amount_needed = 120;
+
+	get_requirements(&d, &iron_ingot);
+
+	calculate_power_consumption(&d);
+
+	print_item_requirements(&d, &iron_ingot);
+	print_power_consumption(&d);
 
 	
-
-	iron_ore.requirement = iron_ingot.requirement / iron_ingot.production_amount * iron_ingot.input.iron_ore;
-
 	return (0);
 }
-
-double x = 100 / 30 * 30
--- Programs/SatisfactoryCalculator/satisfactory_calculator.h
+++ Programs/SatisfactoryCalculator/satisfactory_calculator.h
@@ -1,7 +1,19 @@
+#include <stdio.h>
+#include <math.h>
+
+#define ITEMS 100
 #define BUILDINGS 100
 
-enum	quality = { IMPURE, NORMAL, PURE };
-enum	type = { ORE };
+#define ORE 0
+#define ITEM 1
+
+#define SMELTER 0
+#define CONSTRUCTOR 1
+
+#define IRON_ORE 0
+#define IRON_INGOT 1
+
+typedef struct s_item t_item;
 
 typedef struct s_node
 {
@@ -9,20 +21,13 @@ typedef struct s_node
 	int		quality;
 }			t_node;
 
-typedef struct s_ore
-{
-	int		prod_impure;
-	int		prod_normal;
-	int		prod_pure;
-}			t_ore;
-
 typedef struct s_requirement
 {
 	char	*item;
 	int		amount_needed;
 }			t_requirement;
 
-typedef struct S_building
+typedef struct s_building
 {
 	char		*name;
 	int			power_usage_MW;
@@ -31,24 +36,27 @@ typedef struct S_building
 
 typedef struct s_recipe
 {
-	char		*product;
-	int			type;
 	int			production_amount;
 	int			second_interval;
+	t_item		*items_needed[4];
+	int			item_amount_needed[4];
+	t_item		*waste;
 	t_building	building;
 }				t_recipe;
 
 typedef struct s_item
 {
-	char		*name;
+	int			type;
 	int			recipe_amount;
 	t_recipe	recipes[3];
-}				t_node;
+	int			amount_needed;
+}				t_item;
 
 typedef struct s_data
 {
+	t_item			items[ITEMS];
 	t_building		buildings[BUILDINGS];
 	int				power_consumption;
 	int				max_conveyor_speed;
-	T_requirement	player_requirement;
+	t_requirement	player_requirement;
 }				t_data;
